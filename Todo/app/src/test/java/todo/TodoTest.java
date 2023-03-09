20bcff2c2e0c3863f8a083c97ee938927f8e31a6
Eliza Thomas <elizamthomas1994@gmail.com>
2023-03-09 23:31:34 +0000

    Successfully wrote and passed test for getTasks method of todo list

--- Todo/app/src/test/java/todo/TodoTest.java
+++ Todo/app/src/test/java/todo/TodoTest.java
@@ -10,6 +10,7 @@ public class TodoTest {
   @Test 
   public void beginsWithEmptyList() {
     Todo todo = new Todo();
+
     assertTrue("initial todo list is empty", todo.todos.isEmpty());
   }
 
@@ -19,6 +20,24 @@ public class TodoTest {
 
     ArrayList<String> todoList = new ArrayList<String>();
     todoList.add("cook dinner");
+
     assertEquals("Should return new added task", todoList, todo.addTask("cook dinner"));
+  }
+
+  @Test 
+  public void returnsEntireListOfItems() {
+    Todo todo = new Todo();
+
+    ArrayList<String> todoList = new ArrayList<String>();
+
+    todoList.add("cook dinner");
+    todoList.add("clean the dishes");
+    todoList.add("wash the car");
+
+    todo.addTask("cook dinner");
+    todo.addTask("clean the dishes");
+    todo.addTask("wash the car");
+
+    assertEquals("Should return all added task", todoList, todo.getTasks());
   }
 }
